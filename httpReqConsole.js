f636c17cc491abb6ff4234e47150987ea4f3f778
Jeff Araujo <jeffaraujo.dev@outlook.com>
2023-09-10 20:28:16 +0000

    Update httpReqConsole.js

--- httpReqConsole.js
+++ httpReqConsole.js
@@ -10,19 +10,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/req-debug-sacred-frequencies', function (req, res) {
-
-    const dataFormatada = moment().tz("America/Sao_Paulo").format('DD/MM/YYYY HH:mm:ss');
-
-    const urlFormatada = req.originalUrl.split('fullURL=')[1];
-
-    console.log("[Req Debug] URL da Request:")
-    console.log(`Data: ${dataFormatada} - URL: ${urlFormatada}`);
-
-    res.sendStatus(200);
-});
-
-app.get('/req-debug-conecta-moda', function (req, res) {
+app.get('/req-debug', function (req, res) {
 
     const dataFormatada = moment().tz("America/Sao_Paulo").format('DD/MM/YYYY HH:mm:ss');
 
